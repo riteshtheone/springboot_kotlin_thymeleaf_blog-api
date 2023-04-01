76ca7088cd4da89c5646c08263aeaada8c6392c0
Ritesh Kumar <riteshnagwan@gmail.com>
2023-04-01 15:41:00 +0530

    working fine with fetchtype_lazy

--- src/main/kotlin/com/realtime/blog_api/services/impl/PostServiceImpl.kt
+++ src/main/kotlin/com/realtime/blog_api/services/impl/PostServiceImpl.kt
@@ -4,9 +4,7 @@ import com.realtime.blog_api.beans.PostBean
 import com.realtime.blog_api.dao.CategoryRepository
 import com.realtime.blog_api.dao.PostRepository
 import com.realtime.blog_api.dao.UserRepository
-import com.realtime.blog_api.entities.Category
 import com.realtime.blog_api.entities.Post
-import com.realtime.blog_api.entities.User
 import com.realtime.blog_api.exceptions.ResourceNotFoundException
 import com.realtime.blog_api.services.PostService
 import org.modelmapper.ModelMapper
@@ -22,37 +20,12 @@ class PostServiceImpl(
     @Autowired private val categoryRepository: CategoryRepository) : PostService {
 
     override fun createPost(postBean: PostBean, userId: Int, categoryId: Int): PostBean {
-        val user: User = this.userRepository.findById(userId).orElseThrow { ResourceNotFoundException("User", "user id", userId) }
-        val category: Category = this.categoryRepository.findById(categoryId).orElseThrow { ResourceNotFoundException("Category", "category id", categoryId) }
         val post: Post = this.modelMapper.map(postBean, Post::class.java)
         post.image = "default.png"
         post.postDate = Date()
-        post.user = user
-        post.category = category
-        val savedPost: Post = this.postRepository.save(post)
-        return this.modelMapper.map(savedPost, PostBean::class.java)
-
-
-
-
-
-
-
-
-
-//        postBean.image = "default.png"
-//        postBean.postDate = Date()
-//        postBean.user = this.modelMapper.map(user, UserBean::class.java)
-//        postBean.category = this.modelMapper.map(category, CategoryBean::class.java)
-//        val post = Post()
-//        post.title = postBean.title
-//        post.content = postBean.content
-//        post.image = postBean.image
-//        post.postDate = postBean.postDate
-//        post.user = this.modelMapper.map(postBean.user, User::class.java)
-//        post.category = this.modelMapper.map(postBean.category, Category::class.java)
-//        val newPost = this.postRepository.save(post)
-//        return this.modelMapper.map(newPost, PostBean::class.java)
+        post.user = this.userRepository.findById(userId).orElseThrow { ResourceNotFoundException("User", "user id", userId) }
+        post.category = this.categoryRepository.findById(categoryId).orElseThrow { ResourceNotFoundException("Category", "category id", categoryId) }
+        return this.modelMapper.map(this.postRepository.save(post), PostBean::class.java)
     }
 
     override fun updatePost(postBean: PostBean, postId: Int): PostBean {
