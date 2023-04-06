b6cf804dcc9beb336206c0299ed2aca16c4c501b
Ritesh Kumar <riteshnagwan@gmail.com>
2023-04-06 17:33:08 +0530

    .

--- src/main/kotlin/com/realtime/blog_api/exceptions/ResourceNotFoundException.kt
+++ src/main/kotlin/com/realtime/blog_api/exceptions/ResourceNotFoundException.kt
@@ -1,4 +1,4 @@
 package com.realtime.blog_api.exceptions
 
-class ResourceNotFoundException(private var resourceName: String, private var fieldName: String, private var fieldValue: Int) :
+class ResourceNotFoundException(resourceName: String, fieldName: String, fieldValue: Int) :
     RuntimeException(String.format("%s not found with %s : %s", resourceName, fieldName, fieldValue))
